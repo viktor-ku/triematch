adb4df1ce59b0a9fa35fe0b293d4319d35d24318
kuroljov <viktor.konsta@gmail.com>
2017-06-13 00:58:23 +0300

    Test get function more advanced

--- test/get.js
+++ test/get.js
@@ -8,13 +8,31 @@ const Store = require('../src/Trie')
 const $ = require('./lib/state')
 
 t.test('get', t => {
-  const store = new Store()
-  $.feed(store, $.state)
+  t.test('falsy', t => {
+    const store = new Store()
+    $.feed(store, $.state)
 
-  t.notOk(store.get('Michael'))
-  t.notOk(store.get('foo'))
-  t.notOk(store.get(null))
-  t.notOk(store.get([]))
-  t.equal(store.get($.MichaelJackson).name, $.MichaelJackson)
+    t.notOk(store.get($.Michael + ' J'))
+    t.notOk(store.get('foo'))
+    t.notOk(store.get(null))
+    t.notOk(store.get([]))
+    t.equal(store.get($.MichaelJackson).name, $.MichaelJackson)
+
+    t.end()
+  })
+
+  t.test('truthy', t => {
+    const store = new Store()
+    $.feed(store, $.state)
+
+    $.names.forEach(name => {
+      const person = $.state.filter(x => x.name === name)[0]
+      const result = store.get(name)
+      t.deepEqual(result, person, name)
+    })
+
+    t.end()
+  })
+
   t.end()
 })
