4dd38dbd75a1b1cd601bcbfaeb633262ac5436dc
kuroljov <viktor.konsta@gmail.com>
2017-06-24 05:27:16 +0300

    Change benchmarks onComplete info; Remove benchmark old fill function and replace it with pregenerated test data (in files); Add prefilled stores to test functions

--- benchmark/lib/createBenchmark.js
+++ benchmark/lib/createBenchmark.js
@@ -5,6 +5,20 @@ const Benchmark = require('benchmark')
 const Ora = require('ora')
 const onComplete = require('./onComplete')
 const onStart = require('./onStart')
+const Store = require('../../src/Trie')
+
+const data100 = require('../../data/store-100.json')
+const data1k = require('../../data/store-1k.json')
+const data50k = require('../../data/store-50k.json')
+
+const store0 = new Store()
+const store100 = new Store()
+const store1k = new Store()
+const store50k = new Store()
+
+data100.forEach(x => store100.set(x.number, x))
+data1k.forEach(x => store1k.set(x.number, x))
+data50k.forEach(x => store50k.set(x.number, x))
 
 type args = {
   name: string,
@@ -24,7 +38,14 @@ function createBenchmark (args: args) {
     onComplete: onComplete(spinner)
   }
 
-  const benchmark = new Benchmark(args.test(), options)
+  const testArgs = {
+    store0,
+    store100,
+    store1k,
+    store50k
+  }
+
+  const benchmark = new Benchmark(args.test(testArgs), options)
 
   return () => new Promise((resolve, reject) => {
     benchmark.on('complete', resolve)
--- benchmark/lib/onComplete.js
+++ benchmark/lib/onComplete.js
@@ -1,27 +1,15 @@
 // @flow
 'use strict'
 
-const os = require('os')
 const chalk = require('chalk')
 
 const onComplete = (spinner: Object) => (e: Object) => {
   const bench = e.target
-  const hz = chalk.green(parseInt(bench.hz, 10))
   const elapsed = chalk.green(bench.times.elapsed)
-  const period = chalk.green(bench.times.period * 1000)
-  const count = chalk.green(bench.count)
 
   spinner.succeed(`Finished in ${elapsed} seconds`)
 
-  process.stdout.write(os.EOL)
-  console.log('   ', chalk.blue(bench.name))
-  process.stdout.write(os.EOL)
-
-  console.log('    Executions per second:', hz, 'times')
-  console.log('    The number of times a test was executed:', count, 'times')
-  console.log('    The time taken to execute the test once:', period, 'ms')
-
-  process.stdout.write(os.EOL)
+  console.log('    %s', chalk.blue(bench.toString()))
 }
 
 module.exports = onComplete
